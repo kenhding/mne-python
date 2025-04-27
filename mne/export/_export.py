8402b014116c22a0a192f6bf73415a18bb13ffc8
kenhding <34337748+kenhding@users.noreply.github.com>
2025-04-27 19:45:03 +0200

    Update _export.py

--- mne/export/_export.py
+++ mne/export/_export.py
@@ -17,8 +17,7 @@ def export_raw(
     add_ch_type=False,
     *,
     overwrite=False,
-    verbose=None,
-    unit=dict( eeg="uV", ecog="uV", seeg="uV", eog="uV", ecg="uV", emg="uV", bio="uV", dbs="uV" )
+    verbose=None
 ):
     """Export Raw to external formats.
 
